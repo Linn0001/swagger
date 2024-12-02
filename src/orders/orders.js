ce22a993d63814e2bd54d60dc9b8ef4f21219481
Linn <136191464+Linn0001@users.noreply.github.com>
2024-12-01 19:14:52 -0500

    Delete src directory

--- src/orders/orders.js
+++
@@ -1,7 +0,0 @@
-module.exports.handler = async (event, context) => {
-    let response = {
-        statusCode: 200,
-        body: JSON.stringify('Hello from Lambda! Orders Response')
-    };
-    return response;
-};
