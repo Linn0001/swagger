68e2787550890164e15fbf48cbdd0a4a096650e6
Linn0001 <ariana.mercado@utec.edu.pe>
2024-12-04 21:46:10 -0500

    oa

--- app.js
+++ app.js
@@ -1,49 +1,63 @@
-const express = require('express');
-const swaggerUi = require('swagger-ui-express');
-const fs = require('fs');
-const path = require('path');
-const yaml = require('js-yaml');
+import express from 'express';
+import swaggerUi from 'swagger-ui-express';
+import { readFileSync, readdirSync } from 'fs';
+import { join } from 'path';
+import yaml from 'js-yaml';
 
 const app = express();
 
 const swaggerFilesPath = './swagger-files';
 
-function loadSwagger(fileName) {
-  const filePath = path.join(swaggerFilesPath, fileName);
-  if (!fs.existsSync(filePath)) {
-    throw new Error(`El archivo Swagger "${fileName}" no existe.`);
-  }
-  const fileContent = fs.readFileSync(filePath, 'utf8');
-  return yaml.load(fileContent);
+function loadSwaggerFiles() {
+  const files = readdirSync(swaggerFilesPath).filter(file => file.endsWith('.yaml'));
+  const swaggerDocs = {};
+
+  files.forEach(file => {
+    const name = file.replace('.yaml', '');
+    const filePath = join(swaggerFilesPath, file);
+    const fileContent = readFileSync(filePath, 'utf8');
+    swaggerDocs[name] = yaml.load(fileContent);
+  });
+
+  return swaggerDocs;
 }
 
-app.use('/docs/user', swaggerUi.serve, swaggerUi.setup(loadSwagger('user.yaml')));
-app.use('/docs/song', swaggerUi.serve, swaggerUi.setup(loadSwagger('song.yaml')));
-app.use('/docs/post', swaggerUi.serve, swaggerUi.setup(loadSwagger('post.yaml')));
-app.use('/docs/album', swaggerUi.serve, swaggerUi.setup(loadSwagger('album.yaml')));
-app.use('/docs/playlist', swaggerUi.serve, swaggerUi.setup(loadSwagger('playlist.yaml')));
-app.use('/docs/comment', swaggerUi.serve, swaggerUi.setup(loadSwagger('comment.yaml')));
-app.use('/docs/artist', swaggerUi.serve, swaggerUi.setup(loadSwagger('artist.yaml')));
+const swaggerDocs = loadSwaggerFiles();
 
-// Ruta base para mostrar un mensaje genérico
-app.get('/docs', (req, res) => {
+const swaggerUiOptions = {
+  explorer: true,
+  swaggerOptions: {
+    urls: Object.keys(swaggerDocs).map(name => ({
+      name: name.charAt(0).toUpperCase() + name.slice(1),
+      url: `/swagger/${name}`
+    }))
+  }
+};
+
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(null, swaggerUiOptions));
+
+app.get('/swagger/:name', (req, res) => {
+  const name = req.params.name;
+  if (swaggerDocs[name]) {
+    res.json(swaggerDocs[name]);
+  } else {
+    res.status(404).send('Swagger file not found');
+  }
+});
+
+// Base route to show a generic message
+app.get('/', (req, res) => {
   res.send(`
-    <h1>Documentación API</h1>
-    <p>Accede a la documentación específica:</p>
+    <h1>API Documentation</h1>
+    <p>Access the Swagger UI documentation:</p>
     <ul>
-      <li><a href="/docs/user">Documentación de Usuarios</a></li>
-      <li><a href="/docs/song">Documentación de Canciones</a></li>
-      <li><a href="/docs/post">Documentación de Posteos</a></li>
-      <li><a href="/docs/album">Documentación de Albumnes</a></li>
-      <li><a href="/docs/playlist">Documentación de Playlists</a></li>
-      <li><a href="/docs/comment">Documentación de Comentarios</a></li>
-      <li><a href="/docs/artist">Documentación de Artistas</a></li>
+      <li><a href="/docs">Swagger UI Documentation</a></li>
     </ul>
   `);
 });
 
-// Iniciar el servidor
-const PORT = 3000;
+// Start the server
+const PORT = 4000;
 app.listen(PORT, () => {
-  console.log(`Servidor corriendo en http://localhost:${PORT}/docs`);
+  console.log(`Server running at http://localhost:${PORT}`);
 });
